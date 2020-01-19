41c282b15de7accf2fd6bf33696e6721370580bf
GarySimons <garycsimons@gmail.com>
2020-01-19 17:18:16 +0000

    Added Eat images. Playing with jQuery on Eat

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,44 @@
 $(document).ready(function() {
 
+/* ******* NAV BAR START ******** */
+
+/* Nav Bar logo */
+    $("#navbarbrand").click(function(){
+    $("#home").show();
+    $("#eat").hide();
+    $("#map").hide();
+    $("#footer").show();
+    });    
+
+/* Nav Bar Home */
+    $("#navhome").click(function(){
+    $("#home").show();
+    $("#eat").hide();
+    $("#map").hide();
+    $("#footer").show();
+    }); 
+
+/* Nav Bar Eat */
+    $("#naveat").click(function(){
+    $("#eat").show();
+    $("#home").hide();
+    $("#map").hide();
+    $("#footer").hide();
+    }); 
+
+/* ******* NAV BAR END ******** */
+
+
+
+
+/* ******* EAT START ******** */
+
  /* Show Eat Page */
     $("#buttoneat").click(function(){
     $("#eat").show();
     $("#home").hide();
     $("#map").hide();
+    $("#footer").hide();
     });
 
 /* Show map01 */
@@ -12,13 +46,10 @@ $(document).ready(function() {
     $("#map").show();
     $("#home").hide();
     $("#eat").hide();
+    $("#footer").hide();
     });
 
-/* Nav Bar Eat */
-    $("#naveat").click(function(){
-    $("#eat").show();
-    $("#home").hide();
-    $("#map").hide();
-    }); 
+/* ******* EAT END ******** */
+
 
 }); 
