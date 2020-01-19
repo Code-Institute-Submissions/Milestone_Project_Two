443aa0e558eae02d884cc2e4e6842f544f4cdb4f
GarySimons <garycsimons@gmail.com>
2020-01-19 21:34:28 +0000

    Added images and text to eat pages. Linked all pages with jQuery

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,15 @@ $(document).ready(function() {
     $("#navbarbrand").click(function(){
     $("#home").show();
     $("#eat").hide();
-    $("#map").hide();
+    $("#mapeat01").hide();
+    $("#mapeat02").hide();
+    $("#mapeat03").hide();
+    $("#mapeat04").hide();
+    $("#mapeat05").hide();
+    $("#mapeat06").hide();
+    $("#mapeat07").hide();
+    $("#mapeat08").hide();
+    $("#mapeat09").hide();
     $("#footer").show();
     });    
 
@@ -14,7 +22,15 @@ $(document).ready(function() {
     $("#navhome").click(function(){
     $("#home").show();
     $("#eat").hide();
-    $("#map").hide();
+    $("#mapeat01").hide();
+    $("#mapeat02").hide();
+    $("#mapeat03").hide();
+    $("#mapeat04").hide();
+    $("#mapeat05").hide();
+    $("#mapeat06").hide();
+    $("#mapeat07").hide();
+    $("#mapeat08").hide();
+    $("#mapeat09").hide();
     $("#footer").show();
     }); 
 
@@ -22,7 +38,15 @@ $(document).ready(function() {
     $("#naveat").click(function(){
     $("#eat").show();
     $("#home").hide();
-    $("#map").hide();
+    $("#mapeat01").hide();
+    $("#mapeat02").hide();
+    $("#mapeat03").hide();
+    $("#mapeat04").hide();
+    $("#mapeat05").hide();
+    $("#mapeat06").hide();
+    $("#mapeat07").hide();
+    $("#mapeat08").hide();
+    $("#mapeat09").hide();
     $("#footer").hide();
     }); 
 
@@ -37,13 +61,157 @@ $(document).ready(function() {
     $("#buttoneat").click(function(){
     $("#eat").show();
     $("#home").hide();
-    $("#map").hide();
+    $("#mapeat01").hide();
+    $("#mapeat02").hide();
+    $("#mapeat03").hide();
+    $("#mapeat04").hide();
+    $("#mapeat05").hide();
+    $("#mapeat06").hide();
+    $("#mapeat07").hide();
+    $("#mapeat08").hide();
+    $("#mapeat09").hide();
     $("#footer").hide();
     });
 
-/* Show map01 */
+/* Show mapeat01 */
     $("#eat01").click(function(){
-    $("#map").show();
+    $("#mapeat01").show();
+    $("#mapeat02").hide();
+    $("#mapeat03").hide();
+    $("#mapeat04").hide();
+    $("#mapeat05").hide();
+    $("#mapeat06").hide();
+    $("#mapeat07").hide();
+    $("#mapeat08").hide();
+    $("#mapeat09").hide();
+    $("#home").hide();
+    $("#eat").hide();
+    $("#footer").hide();
+    });
+
+/* Show mapeat02 */
+    $("#eat02").click(function(){
+    $("#mapeat01").hide();
+    $("#mapeat02").show();
+    $("#mapeat03").hide();
+    $("#mapeat04").hide();
+    $("#mapeat05").hide();
+    $("#mapeat06").hide();
+    $("#mapeat07").hide();
+    $("#mapeat08").hide();
+    $("#mapeat09").hide();
+    $("#home").hide();
+    $("#eat").hide();
+    $("#footer").hide();
+    });
+
+/* Show mapeat03 */
+    $("#eat03").click(function(){
+    $("#mapeat01").hide();
+    $("#mapeat02").hide();
+    $("#mapeat03").show();
+    $("#mapeat04").hide();
+    $("#mapeat05").hide();
+    $("#mapeat06").hide();
+    $("#mapeat07").hide();
+    $("#mapeat08").hide();
+    $("#mapeat09").hide();
+    $("#home").hide();
+    $("#eat").hide();
+    $("#footer").hide();
+    });
+
+/* Show mapeat04 */
+    $("#eat04").click(function(){
+    $("#mapeat01").hide();
+    $("#mapeat02").hide();
+    $("#mapeat03").hide();
+    $("#mapeat04").show();
+    $("#mapeat05").hide();
+    $("#mapeat06").hide();
+    $("#mapeat07").hide();
+    $("#mapeat08").hide();
+    $("#mapeat09").hide();
+    $("#home").hide();
+    $("#eat").hide();
+    $("#footer").hide();
+    });
+
+    /* Show mapeat05 */
+    $("#eat05").click(function(){
+    $("#mapeat01").hide();
+    $("#mapeat02").hide();
+    $("#mapeat03").hide();
+    $("#mapeat04").hide();
+    $("#mapeat05").show();
+    $("#mapeat06").hide();
+    $("#mapeat07").hide();
+    $("#mapeat08").hide();
+    $("#mapeat09").hide();
+    $("#home").hide();
+    $("#eat").hide();
+    $("#footer").hide();
+    });
+
+    /* Show mapeat06 */
+    $("#eat06").click(function(){
+    $("#mapeat01").hide();
+    $("#mapeat02").hide();
+    $("#mapeat03").hide();
+    $("#mapeat04").hide();
+    $("#mapeat05").hide();
+    $("#mapeat06").show();
+    $("#mapeat07").hide();
+    $("#mapeat08").hide();
+    $("#mapeat09").hide();
+    $("#home").hide();
+    $("#eat").hide();
+    $("#footer").hide();
+    });
+
+    /* Show mapeat07 */
+    $("#eat07").click(function(){
+    $("#mapeat01").hide();
+    $("#mapeat02").hide();
+    $("#mapeat03").hide();
+    $("#mapeat04").hide();
+    $("#mapeat05").hide();
+    $("#mapeat06").hide();
+    $("#mapeat07").show();
+    $("#mapeat08").hide();
+    $("#mapeat09").hide();
+    $("#home").hide();
+    $("#eat").hide();
+    $("#footer").hide();
+    });
+
+    /* Show mapeat08 */
+    $("#eat08").click(function(){
+    $("#mapeat01").hide();
+    $("#mapeat02").hide();
+    $("#mapeat03").hide();
+    $("#mapeat04").hide();
+    $("#mapeat05").hide();
+    $("#mapeat06").hide();
+    $("#mapeat07").hide();
+    $("#mapeat08").show();
+    $("#mapeat09").hide();
+    $("#home").hide();
+    $("#eat").hide();
+    $("#footer").hide();
+    });
+
+    /* Show mapeat09 */
+    $("#eat09").click(function(){
+    $("#mapeat01").hide();
+    $("#mapeat02").hide();
+    $("#mapeat03").hide();
+    $("#mapeat04").hide();
+    $("#mapeat05").hide();
+    $("#mapeat06").hide();
+    $("#mapeat07").hide();
+    $("#mapeat08").hide();
+    $("#mapeat09").show();
     $("#home").hide();
     $("#eat").hide();
     $("#footer").hide();
