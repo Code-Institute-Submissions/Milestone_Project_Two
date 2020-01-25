5b360e565f8aa420dd3058e6b323ee290b84e71f
GarySimons <garycsimons@gmail.com>
2020-01-25 17:24:55 +0000

    hiding and showing sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,39 @@ $(document).ready(function() {
 
 // ----------- NAV BAR ---------
 
-// On load, hide all sections apart from 'home'
+// On load, hide all sections apart from 'home and footer'
 $(function() {
-    $("#eat, #mapeat01").hide();
+    $("#eat, #drink, #stay, #mapeat01").hide();
 
     // When each nav li is clicked, hide all sections.
-		$("#navhome, #naveat, #navdrink, #stay").on("click", function () {
+		$("#navbarbrand, #navhome, #naveat, #navdrink, #stay").on("click", function () {
 			$("#home, #eat, #drink").hide();
 
 			// Remove the 'selected' class from all li elements.
 			$("#navhome, #naveat, #navdrink, #stay").removeClass('active');
 
 			// If/else statements to check which li was clicked,
-			// apply 'selected' class only to that li, and fadeIn the corresponding section.
+			// apply 'selected' class only to that li, and show the corresponding section.
 			if ($(this).attr("id") == "navhome") {
 				$("#navhome").addClass('active');
-				$("#home").show();
+                $("#home, footer").show();
+			} else if ($(this).attr("id") == "naveat") {
+				$("#naveat").addClass('active');
+                $("#eat").show();
+			} else if ($(this).attr("id") == "navdrink") {
+				$("#navdrink").addClass('active');
+				$("#drink").show();
+			} else if ($(this).attr("id") == "navstay") {
+				$("#navstay").addClass('active');
+                $("#stay").show();
+                
+            // If/else statements to check which box was clicked,
+			// apply 'selected' class only to that li, and show the corresponding section.      
+
+            if ($(this).attr("id") == "navbarbrand") {
+				$("#navhome").addClass('active');
+                $("#home").show();
+                $("#footer").show();
 			} else if ($(this).attr("id") == "naveat") {
 				$("#naveat").addClass('active');
 				$("#eat").show();
@@ -26,7 +43,8 @@ $(function() {
 				$("#drink").show();
 			} else if ($(this).attr("id") == "navstay") {
 				$("#navstay").addClass('active');
-				$("#stay").show();
+                $("#stay").show();
+            }
 			}
 		});
 	});
